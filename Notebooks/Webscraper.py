22baf51b18f06b72981d739ad6d0058d7478365d
brian-greeley <66392875+brian-greeley@users.noreply.github.com>
2020-08-17 16:56:18 -0400

    Change Webscraper to Selenium

--- Notebooks/Webscraper.py
+++ Notebooks/Webscraper.py
@@ -14,3 +14,26 @@ def getSource(url):
     req = requests.get(url)
     return req.text
 
+
+# In[3]:
+
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+
+
+# In[4]:
+
+
+def getArticles(url):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("useAutomationExtension", False)
+    chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
+    driver = webdriver.Chrome(options=chrome_options,executable_path='C:\Windows\Drivers\chromedriver')
+    driver.get(url)
+    articles = WebDriverWait(driver, timeout=20).until(lambda d: d.find_elements_by_tag_name('article'))
+    driver.close()
+    return articles
+
